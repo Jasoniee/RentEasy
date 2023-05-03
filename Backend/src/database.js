f784b3cfa0e77768580a8bf253a96fc3d8a05426
tz275 <tz275@cornell.edu>
2023-05-02 20:03:39 -0400

    1

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -5,31 +5,6 @@ const Post = require("./Post")
 mongo.connect("mongodb://localhost/appdb")
 
 const addUser = async (nick_name, user_name, password, info) => {
-<<<<<<< HEAD
-    const user = await User.create({
-        nick_name: nick_name,
-        user_name: user_name,
-        password: password,
-        info: info,
-    })
-    return user
-}
-const addPost=async(start_date, end_date, price, description,email)=>{
-    const post = await Post.create({
-        start_date:start_date,
-        end_date: end_date,
-        price:price,
-        description:description,
-        email:email
-    })
-}
-module.exports={
-    addUser,
-    addPost
-}
-const user1 = addPost("2022-05-20","2022-08-20",1600,"I love apartment",'qny2@cornell.edu')
-
-=======
     try {
         const user = await User.create({
             nick_name: nick_name,
@@ -55,4 +30,3 @@ run()
 module.exports = {
     addUser,
 }
->>>>>>> 1859bb298d9f56028bdc460dd469d9dbc15a53c0
